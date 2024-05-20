54945fa90433aabf62ac4a05845452f25a9cbec1
Devesh Shukla <shukladevesh9814@gmail.com>
2024-05-21 00:12:25 +0530

    Connected to the database.

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to the Mongo DB');
+}).catch((err) => {
+    console.log(err);
+});
+
+
 const app = express();
 
 app.listen(3000,() =>{
     console.log("Server is listening to the port 3000")
 });
+
+
+//VVkrmjbTjsAeCZ8f
