4eb907500ac88dedcf78c72c22fa265e843452be
Devesh Shukla <shukladevesh9814@gmail.com>
2024-05-20 11:17:02 +0530

    Created Header and Server and also run it.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import { BrowserRouter ,Route ,Router, Routes } from "react-router-dom"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Header from "./Components/Header"
+
 
 export default function App() {
   return (
-    <h1 className='text-red-500'> 
-      Devesh
-    </h1>
+    <BrowserRouter>
+    <Header/>
+     <Routes>
+        <Route path="/" element = {<Home/>}/>
+        <Route path="/About" element = {<About/>}/>
+        <Route path="/SignIn" element = {<SignIn/>}/>
+        <Route path="/SignUp" element = {<SignUp/>}/>
+        <Route path="/Profile" element = {<Profile/>}/>
+
+     </Routes>
+    </BrowserRouter>
   )
 }
