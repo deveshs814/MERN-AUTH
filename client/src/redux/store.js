a0b9fccb5e7bcee8381bbdabf3846852e9510eaa
Devesh Shukla <shukladevesh9814@gmail.com>
2024-05-31 23:57:33 +0530

    Completed sign in page functionality and added redux toolkit.

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,1 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+    reducer:{},
+    middleware:(getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false,
+    }),
+});
